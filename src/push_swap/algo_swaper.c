b491c08a8abd60a0f6d509159f5067c4d6f0da9e
vlaroque <arrageois@gmail.com>
2019-07-30 16:27:07 +0200

    function head swaper done

--- src/push_swap/algo_swaper.c
+++ src/push_swap/algo_swaper.c
@@ -1,5 +1,32 @@
 #include "push_swap.h"
 
+static int	head_operations(t_tab *tab)
+{
+	if (tab->a->start->index > tab->a->start->next->index)
+		swap_a(tab);
+	if (tab->a->start->prev->data == PIVOT)
+	{
+		if (tab->a->start->prev->index + 1 == tab->a->start->index)
+		{
+			tab->a->start->data = PIVOT;
+			if (tab->a->start->index + 1 == tab->a->start->next->index)
+				tab->a->start->next->data = PIVOT;
+		}
+	}
+	if (tab->a->start->next->next->data == PIVOT)
+	{
+		if (tab->a->start->next->next->index - 1 == tab->a->start->next->index)
+		{
+			tab->a->start->next->data = PIVOT;
+			if (tab->a->start->next->index - 1 == tab->a->start->index)
+				tab->a->start->data = PIVOT;
+		}
+	}
+	if (tab->a->start->next->next->data == PIVOT && tab->a->start->next->data == PIVOT && tab->a->start->data == PIVOT && tab->a->start->prev->data == PIVOT)
+		return (1);
+	return (0);
+}
+
 static int	dist_swap(t_tab *tab)
 {
 	t_elem *head;
